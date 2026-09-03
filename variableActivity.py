b60180db64d12337a0caec1206d4eae88a547269
Ashe <amondesir878@boyslatin.net>
2026-09-03 13:43:14 +0000

    new notes

---
+++ variableActivity.py
@@ -0,0 +1,44 @@
+# Variables- a container that stores data. you can
+#name your container almost anything you want, so long 
+#as it is descriptive.
+
+name="Ashe Mon"
+yearsofExp=10
+Graduate=False
+age=15
+#when we want to create a variable, we make up a word, 
+#use the equal sign and then assign it a data type
+#whenever the computer sees our variable word it will
+#always use the data inside
+print("hello",name)
+print("you are a minor")
+
+#variable naming rules 
+# 1.A variable must start with letter and cannot start with a number
+# 2.A variable can be a mixed of letters and numbers But not symbols
+# 3.A variable cannot be a Python keyword
+
+
+Numberofapples=80
+mathstudentscount=20
+insuranceadd=2800
+
+priceofcoke=4
+priceofsprite=3
+priceofpepesi=4
+
+bonzemembership=12
+silvermembership=26
+goldmemberprice=34
+
+#camal case-when creating variable names with multiple words,
+#every new word begins with a capital letter
+#the reason why its called camal case is bc every new word
+#represents a hump
+
+# snake case- when creating a variable using snake case
+# we use a undercore symbols to space out each new word
+# the reason why its called snake case is bc the umdercore
+# represents a snake on the ground new_word_
+
+
