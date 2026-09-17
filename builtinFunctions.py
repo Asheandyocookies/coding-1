f4e6a1693c7f3b24bd1707ac289bb45a8c240a0a
Ashe <amondesir878@boyslatin.net>
2026-09-17 13:43:04 +0000

    notes/ new unit

---
+++ builtinFunctions.py
@@ -0,0 +1,19 @@
+# function - simply put; a code block of
+# instructions for computers to follow.
+
+#built- in function- a code block of 
+# instructions for a computer to follow
+# that was already written for us
+# PRE-WRITTEN CODE INSTRUCTIONS
+
+print()
+year = input()
+print(year)
+
+# input() a built in function that allows a user to 
+# type in a data into the terminal. the data that is passed
+# in will always be treated as a string data type.
+#print() a built in function that allows a user to 
+# print data out in the terminal.
+
+
